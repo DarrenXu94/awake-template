53ef60ccc585540f458112ab5162ee4aa3b7ae63
Darren Xu <darrenxu1994@gmail.com>
2023-03-08 17:29:41 +1100

    Parse in map points

--- pages/map.vue
+++ pages/map.vue
@@ -1,23 +1,22 @@
 <template>
   <div class="page-wrapper map-page container">
-    <!-- <site-hero
-      title="Map"
-      subtitle="Where is everything?"
-      image="/uploads/lunch.jpeg"
-    ></site-hero> -->
-
     <div id="map-wrap" style="height: 100%">
       <client-only>
-        <l-map :zoom="13" :center="[-35.28608957097441, 149.11992525975597]">
+        <l-map :zoom="16" :center="[-35.28608957097441, 149.11992525975597]">
           <l-tile-layer
             url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
           ></l-tile-layer>
-          <!-- <l-marker :lat-lng="[55.9464418, 8.1277591]"></l-marker> -->
           <l-marker
             v-for="point in locations"
-            :key="point[0]"
-            :lat-lng="point"
-          ></l-marker>
+            :key="point.name"
+            :lat-lng="point.latlon"
+          >
+            <l-popup>
+              <div>
+                {{ point.name }}
+              </div>
+            </l-popup>
+          </l-marker>
         </l-map>
       </client-only>
     </div>
@@ -25,7 +24,10 @@
 </template>
 
 <script>
-const locations = [[-35.2783192, 149.1270956]]
+const locations = [
+  { name: 'malatang', latlon: [-35.2783192, 149.1270956] },
+  { name: 'terra', latlon: [-35.27863005347709, 149.12695721534584] }
+]
 export default {
   components: {},
   data() {
